aaef01f6353460d071c956b8537b90a016a947d4
Igor K <feling87@gmail.com>
2016-05-15 17:29:40 +0300

    Ready

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,17 @@
 import {Component} from 'angular2/core';
+import {PropertyBindingComponent} from "./property-component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+    <section class="parent">
+      <h2>This is a parent component</h2>
+        <section class="child">
+        <my-property-binding></my-property-binding>
+        </section>
+    </section>
     `,
+    directives: [PropertyBindingComponent]
 })
 export class AppComponent {
 
